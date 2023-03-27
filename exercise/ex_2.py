2ffa5adf9a7965997bf0b720c707f1ad4c059ba2
Dominika Smoczyńska <donka.smoczynska@gmail.com>
2023-03-27 12:12:13 +0200

    Add rectangle

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -7,3 +7,15 @@ pole = pow(a, 2)
 
 tekst = "Obwód kwadratu wynosi {}, a pole {}.".format(obwod, pole)
 print(tekst)
+
+#prostokat
+a_p = 10
+b_p = 5
+obwod_prostokat = 2*a_p+2*b_p
+pole_prostokat = a_p*b_p
+#kolo
+tekst_prostokat = "Obwód prostokąta wynosi {}, a pole {}.".format(obwod_prostokat, pole_prostokat)
+print(tekst_prostokat)
+#trapez
+
+#romb
