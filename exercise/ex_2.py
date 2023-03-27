25291fdff24ed76c8d78752e17429c0eca615fce
Dominika Smoczyńska <donka.smoczynska@gmail.com>
2023-03-27 12:17:02 +0200

    Add circle

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -1,3 +1,4 @@
+import math
 # kwadrat
 
 a = 10
@@ -13,9 +14,15 @@ a_p = 10
 b_p = 5
 obwod_prostokat = 2*a_p+2*b_p
 pole_prostokat = a_p*b_p
-#kolo
 tekst_prostokat = "Obwód prostokąta wynosi {}, a pole {}.".format(obwod_prostokat, pole_prostokat)
 print(tekst_prostokat)
+
+#kolo
+r = 5
+obwod_kolo = math.pi *2*r
+pole_kolo = math.pi * pow(r, 2)
+tekst_kolo = "Obwód koła wynosi {}, a pole {}.".format(obwod_kolo, pole_kolo)
+print(tekst_kolo)
 #trapez
 
 #romb
