e3af062807863270668082de1715aeb380e1094c
Dominika Smoczyńska <donka.smoczynska@gmail.com>
2023-04-12 19:33:53 +0200

    Added zadanie 1.6

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -54,3 +54,17 @@ print("Wynik wynosi: 0")
 
 wynik = pow(3,4)
 print("Wynik wynosi: {}".format(wynik))
+
+# zadanie 1.6
+
+# policz ilosc nawiasow ( w danym ciagu znakow
+
+ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
+liczba_nawiasow_otwierajacych = 0
+
+# oczekiwany rezultat:
+# Liczba nawiasow otwierajacych wynosi: 4
+print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
+
+liczba_nawiasow_otwierajacych = ciag_znakow.count('(')
+print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
