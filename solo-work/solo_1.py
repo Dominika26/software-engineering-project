879b6efd9756ef8f245a49350817e4f847cc1efd
Dominika Smoczyńska <donka.smoczynska@gmail.com>
2023-04-12 19:25:27 +0200

    Added zadanie 1.3

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -13,3 +13,14 @@ student = input("Wpisz swoje imie")
 print("Hello Ola")
 print("{} {}".format(hello, student))
 
+# zadanie 1.3
+
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+
+# policz liczbe studentow w tablicy studenci
+# oczekiwany rezultat: Liczba studentow wynosi: 4
+liczba_studentow = 0
+print("Liczba studentow wynosi: 0")
+
+liczba_studentow = len(studenci)
+print("Liczba studentów wynosi: {}".format(liczba_studentow))
